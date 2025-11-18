8c0d1373dc5d7bb1f359ca4bcffd80e4090eec45
Shirsendu Pramanik <pramanikshirsendu07@gmail.com>
2025-11-18 23:39:34 +0530

    Fix baseURL for API endpoint

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://studentforn-backend-1.onrender.com/api",
+  baseURL: "https://studentform-backend-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// OPTIONAL: Auto attach token if logged in
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error)
